dc84d9d23b9072a47afe36fa41787c4122d5675c
顾叶菁 <gyj_android@sina.com>
2017-02-04 15:42:10 +0800

    1.2.1 调整demo

--- app/src/main/java/com/easy/MainActivity.java
+++ app/src/main/java/com/easy/MainActivity.java
@@ -2,12 +2,42 @@ package com.easy;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView t1,t2,t3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t1=(TextView)findViewById(R.id.tv_code_1);
+        t2=(TextView)findViewById(R.id.tv_code_2);
+        t3=(TextView)findViewById(R.id.tv_code_3);
+        t1.setText("<com.guyj.img.EasyImageView\n" +
+                "        android:layout_width=\"100dp\"\n" +
+                "        android:layout_height=\"100dp\"\n" +
+                "        android:src=\"@mipmap/aaa\"\n" +
+                "        app:type=\"circle\"\n" +
+                "        app:border_width=\"2dp\"\n" +
+                "        app:border_color=\"@color/colorPrimary\"\n" +
+                "        />");
+        t2.setText("<com.guyj.img.EasyImageView\n" +
+                "        android:layout_width=\"200dp\"\n" +
+                "        android:layout_height=\"100dp\"\n" +
+                "        android:src=\"@mipmap/aaa\"\n" +
+                "        app:type=\"rectangle\"\n" +
+                "        app:border_width=\"2dp\"\n" +
+                "        app:border_color=\"@color/colorPrimary\"\n" +
+                "        />");
+        t3.setText("<com.guyj.img.EasyImageView\n" +
+                "        android:layout_width=\"200dp\"\n" +
+                "        android:layout_height=\"100dp\"\n" +
+                "        android:src=\"@mipmap/aaa\"\n" +
+                "        app:type=\"oval\"\n" +
+                "        app:border_width=\"2px\"\n" +
+                "        app:border_color=\"@color/colorPrimary\"\n" +
+                "        />");
     }
 }
